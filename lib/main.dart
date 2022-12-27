0622b8e80edd02035e39d40a2153b1b8fb0d5f24
Pankaj Kumar Das <pdas0354158@gmail.com>
2022-12-27 20:36:33 +0530

    chapter3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,46 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/home.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "awesome app",
     home: HomePage(),
+    theme: ThemeData(primarySwatch: Colors.purple),
   ));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("aswome app"),
-        ),
-        body: Center(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              color: Colors.red,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
-            ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              alignment: Alignment.center,
-              width: 100,
-              height: 100,
-              color: Colors.green,
-            )
-          ],
-        )));
-  }
 }
