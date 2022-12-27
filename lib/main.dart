7ba12cf0a81214c7886a9d12bb8377da42034184
Pankaj Kumar Das <pdas0354158@gmail.com>
2022-12-27 16:02:29 +0530

    chapter 2 copleted

--- lib/main.dart
+++ lib/main.dart
@@ -15,32 +15,32 @@ class HomePage extends StatelessWidget {
           title: Text("aswome app"),
         ),
         body: Center(
-          child: Container(
-            padding: const EdgeInsets.all(8),
-            alignment: Alignment.center,
-            width: 100,
-            height: 100,
-            decoration: BoxDecoration(
-                //shape: BoxShape.circle,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                      color: Color.fromARGB(255, 16, 16, 16),
-                      blurRadius: 5, //spreadRadius: 20
-                      offset: Offset(2.0, 5.0)),
-                ],
-                color: Colors.teal,
-                gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
-            child: Text(
-              "i am a box",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-              ),
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.red,
             ),
-          ),
-        ));
+            Container(
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              alignment: Alignment.center,
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            )
+          ],
+        )));
   }
 }
