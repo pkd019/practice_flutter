659fe9417f8ca867af3bf1c80a243e8efd2ca553
Pankaj Kumar Das <pdas0354158@gmail.com>
2022-12-27 15:51:20 +0530

    decoration of container

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("aswome app"),
-      ),
-      body: Container(
-        child: Center(child: Text("hi flutter")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("aswome app"),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+                //shape: BoxShape.circle,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [
+                  BoxShadow(
+                      color: Color.fromARGB(255, 16, 16, 16),
+                      blurRadius: 5, //spreadRadius: 20
+                      offset: Offset(2.0, 5.0)),
+                ],
+                color: Colors.teal,
+                gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+            child: Text(
+              "i am a box",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+        ));
   }
 }
