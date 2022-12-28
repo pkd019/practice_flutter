5c7eb2c3cc2e489333fba0acfcbd99ea3f9ab659
Pankaj Kumar Das <pdas0354158@gmail.com>
2022-12-28 14:56:37 +0530

    chapter 6 completed

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/drawer.dart';
+import 'package:flutter_application_1/utils/constants.dart';
 
 class MyHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("home page")),
+      appBar: AppBar(
+        title: Text("home page"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: () {
+              constants.pref.setBool("loggedin", false);
+              Navigator.pushReplacementNamed(context, "/login");
+            },
+          ),
+        ],
+      ),
+      drawer: MyDrawer(),
     );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,6 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class constants {
+  // ignore: unnecessary_new
+  static SharedPreferences pref = SharedPreferences as SharedPreferences;
+}
